a74801bc361e877c42aea5861cabd9b0221cf5f5
malak1103 <121356913+malak1103@users.noreply.github.com>
2024-01-14 23:42:32 +0100

    final

--- Projet.c
+++ Projet.c
@@ -1,115 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/raylib.h"
+#include <string.h>
 
 typedef struct Node Node;
 typedef struct Node {
     int data;
     Node* next;
 };
+
 Node* head = NULL;
 
+// Function to insert an element at the end of the list
+void insertAtEnd(int data)
+{
+    Node *newNode = malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
 
-
-// Function to delete an element from the list at a given position k
-void deleteElementAtPosition(int position) {
-    if (head == NULL) {
-        printf("List is empty. Deletion not possible.\n");
-        return;
+    if (head == NULL)
+    {
+        head = newNode;
     }
 
-    Node* temp = head;
+    else
+    {
+        Node *temp = head;
 
-    if (position == 1) {
-        head = temp->next;
-        free(temp);
-    } else {
-        for (int i = 1; i < position - 1 && temp != NULL; ++i) {
+        while (temp->next != NULL)
+        {
             temp = temp->next;
         }
 
-        if (temp != NULL && temp->next != NULL) {
-            Node* toDelete = temp->next;
-            temp->next = toDelete->next;
-            free(toDelete);
-        } else {
-            printf("Invalid position for deletion.\n");
+        temp->next = newNode;
+    }
+}
+
+void createList()
+{
+    head = NULL;
+
+    int data;
+
+    printf("Enter the number of elements in the list: ");
+
+    int numElements;
+    scanf("%d", &numElements);
+
+    for (int i = 0; i < numElements; ++i)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &data);
+
+        insertAtEnd(data);
+    }
+}
+
+// Function to draw a rectangle representing a list node
+void drawNodeRectangle(int posX, int posY, int data, bool highlighted)
+{
+    // Draw the rectangle containing the data
+    DrawRectangle(posX, posY, 50, 50, highlighted ? SKYBLUE : LIGHTGRAY);
+    DrawText(TextFormat("%d", data), posX + 10, posY + 10, 20, BLACK);
+
+    // Draw the arrow
+    DrawLine(posX + 50, posY + 25, posX + 100, posY + 25, BLACK);
+    DrawTriangle((Vector2){posX + 100, posY + 25}, (Vector2){posX + 120, posY + 20}, (Vector2){posX + 120, posY + 30}, BLACK);
+}
+
+// Function to draw the entire linked list
+void drawList(int highlightedNode)
+{
+    Node *current = head;
+
+    int posX = 50;
+    int posY = 200;
+
+    while (current != NULL)
+    {
+        drawNodeRectangle(posX, posY, current->data, (highlightedNode == current->data));
+        posX += 120;
+
+        current = current->next;
+    }
+}
+
+// Function to add an element to the list at a given position k
+void addElementAtPosition(int data, int position)
+{
+    Node *newNode = malloc(sizeof(Node));
+    newNode->data = data;
+
+    if (position == 1)
+    {
+        newNode->next = head;
+        head = newNode;
+    }
+
+    else
+    {
+        Node *temp = head;
+
+        for (int i = 1; i < position - 1 && temp != NULL; ++i)
+        {
+            temp = temp->next;
+        }
+
+        if (temp != NULL)
+        {
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+
+        else
+        {
+            printf("Invalid position for insertion.\n");
+            free(newNode);
         }
     }
 }
 
-// Function to perform bubble sort on the list
-void bubbleSort() {
-    int swapped;
-    Node* ptr1;
-    Node* lptr = NULL;
 
-    if (head == NULL)
-        return;
 
-    do {
-        swapped = 0;
-        ptr1 = head;
 
-        while (ptr1->next != lptr) {
-            if (ptr1->data > ptr1->next->data) {
-                int temp = ptr1->data;
-                ptr1->data = ptr1->next->data;
-                ptr1->next->data = temp;
-                swapped = 1;
-            }
-            ptr1 = ptr1->next;
-        }
-        lptr = ptr1;
-    } while (swapped);
+int isMouseOverButton(int x, int y, int width, int height)
+{
+    Vector2 mouse = GetMousePosition();
+    return (mouse.x >= x && mouse.x <= x + width && mouse.y >= y && mouse.y <= y + height);
 }
-
-    return tete;
-}
-
-int main() {
-   const int screenWidth = 800;
-    const int screenHeight = 600;
-
-    InitWindow(screenWidth, screenHeight, "Raylib LinkedList Visualization");
-
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        // UI
-        DrawText("Press 1: Add Element  2: Delete Element  3: Start Sorting", 10, 10, 20, BLACK);
-
-        if (IsKeyPressed(KEY_ONE)) {
-            int data, position;
-            printf("Enter a number: ");
-            scanf("%d", &data);
-            printf("Enter position to insert: ");
-            scanf("%d", &position);
-            addElementAtPosition(data, position);
-        }
-
-        if (IsKeyPressed(KEY_TWO)) {
-            int position;
-            printf("Enter position to delete: ");
-            scanf("%d", &position);
-            deleteElementAtPosition(position);
-        }
-
-        if (IsKeyPressed(KEY_THREE)) {
-            bubbleSort();
-        }
-
-        drawList();
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-
-    return 0;
-}
-
-
